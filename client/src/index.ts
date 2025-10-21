b9607c15b9cbe0d1db14b8b18f3d67b43f7947c3
Emanuel Lázaro <emanuellzr01@outlook.com>
2025-10-20 21:35:13 -0300

    fix: change index.ts directory

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,6 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { registerRoutes } from "../../server/routes";
+import { setupVite, serveStatic, log } from "../../server/vite";
 
 const app = express();
 app.use(express.json());
