644b1eacdda3281306f8564a4853cacb4930fcc4
spacobellas <chatbotspacodellas@gmail.com>
2026-08-13 14:12:13 -0300

    Add RevistaRedirect component for external redirection

---
+++ src/pages/RevistaRedirect.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+
+export default function RevistaRedirect() {
+  useEffect(() => {
+    window.location.replace("https://heyzine.com/flip-book/ed4522992f.html");
+  }, []);
+
+  return null;
+}
