afb2e4be4ddef5935c687c68acc8eab8563b499e
spacobellas <chatbotspacodellas@gmail.com>
2026-08-13 18:37:54 -0300

    Update redirect URL in RevistaRedirect component

--- src/pages/RevistaRedirect.tsx
+++ src/pages/RevistaRedirect.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function RevistaRedirect() {
   useEffect(() => {
-    window.location.replace("https://heyzine.com/flip-book/ed4522992f.html");
+    window.location.replace("https://heyzine.com/flip-book/8e2692e491.html");
   }, []);
 
   return null;
